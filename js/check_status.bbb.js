18e4e94d0d5bb449d6929fefec0c08b2ccff9378
rpsu <rpsu@102121.no-reply.drupal.org>
2013-07-26 17:53:46 +0200

    Issue #2045613 by rpsu: Automated attend-to JS broken, due to D7 JS API change

--- js/check_status.bbb.js
+++ js/check_status.bbb.js
@@ -1,14 +1,18 @@
-Drupal.behaviors.bbbCheckStatusInit = function () {
-  Drupal.bbbCheckStatus();
-  setInterval("Drupal.bbbCheckStatus();", 5000);
-}
-
-Drupal.bbbCheckStatus = function () {
-  var url = bbb_check_status_url;
-  $.getJSON(url, function(data) {
-    console.log(data);
-    if (data.is_running == true) {
-      location.href = location.href + '/meeting/attend';
+(function($) {
+  Drupal.behaviors.bbbCheckStatusInit = {
+   attach: function (context, settings) {
+    Drupal.bbbCheckStatus();
+    setInterval("Drupal.bbbCheckStatus();", 5000);
     }
-  });
-}
+  }
+
+  Drupal.bbbCheckStatus = function () {
+    var url = bbb_check_status_url;
+    $.getJSON(url, function(data) {
+      console.log(data);
+      if (data.running == true) {
+        location.href = location.href + '/meeting/attend';
+      }
+    });
+  }
+})(jQuery);
